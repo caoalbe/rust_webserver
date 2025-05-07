00ad24178830cea9003219cec1392ce99ff5b846
Albert Cao <albert.cao.1000@gmail.com>
2025-05-07 13:28:38 -0400

    headers on respnse

--- src/models/response.rs
+++ src/models/response.rs
@@ -1,11 +1,12 @@
-use std::{io::Write, net::TcpStream};
+use std::{collections::HashMap, io::Write, net::TcpStream};
 
 pub struct Response {
     stream: TcpStream,
     http_version: String,
     http_status: String,
-    contents: String,
+    headers: HashMap<String, String>,
     headers_sent: bool,
+    contents: String,
 }
 
 impl Response {
@@ -14,9 +15,14 @@ impl Response {
             stream,
             http_version: "HTTP/1.1".to_string(),
             http_status: "200 OK".to_string(),
-            contents: "".to_string(),
+            headers: HashMap::new(),
             headers_sent: false,
+            contents: "".to_string(),
         }
+    }
+
+    pub fn append_header(&mut self, field: String, value: String) -> () {
+        self.headers.insert(field, value);
     }
 
     pub fn set_content(&mut self, new_content: String) -> () {
@@ -24,9 +30,14 @@ impl Response {
     }
 
     pub fn send(&mut self) -> () {
+        let mut http_header: String = String::new();
+        for (key, value) in self.headers.clone() {
+            http_header.push_str(&format!("{}: {}\n", key, value).to_string());
+        }
+
         let output: String = format!(
-            "{} {}\r\n\r\n{}\r\n",
-            self.http_version, self.http_status, self.contents
+            "{} {}\r\n{}\r\n\r\n{}\r\n",
+            self.http_version, self.http_status, http_header, self.contents
         );
         self.stream.write_all(output.as_bytes()).unwrap();
         self.headers_sent = true;
